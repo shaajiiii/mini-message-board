61f3dfe621b51ec8f1fe9f3fb16e96f00e20317a
Manu <manushaji717@gmail.com>
2022-12-14 18:25:36 +0530

    Pages component added. server to be done after designing with custom css. Routing in react done

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login'; 
+import Signup from './pages/Signup/Signup';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      app
+
+    <Routes>
+
+      <Route path="/" exact element={<Home/>} />
+      <Route path="/login" exact element={<Login />} />
+      <Route path="/signup" exact element={<Signup />} />
+     
+    </Routes>
+
     </div>
   );
 }
