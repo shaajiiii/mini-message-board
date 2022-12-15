93ac4abd265ae774c5fba55d5994d2d317774693
Manu <manushaji717@gmail.com>
2022-12-15 16:32:45 +0530

    Nav bar design finished. Main content designing on progress

---
+++ client/src/components/Nav/Nav.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+import './Nav.css';
+// import Swal from 'sweetalert2';
+// import withReactContent from 'sweetalert2-react-content';
+
+
+function Nav() {
+
+    // const [user, setUser] = useState(false)
+    // let checkuser = () => {
+
+    //     let token = localStorage.getItem('token')
+    //     if (token) setUser(true)
+
+    // }
+
+
+    // const MySwal = withReactContent(Swal)
+
+    const LogOutprompt = () => {
+        // MySwal.fire({
+        //     title: 'Log out of Liberty?',
+        //     icon: 'warning',
+        //     showCancelButton: true,
+        //     confirmButtonColor: '#3bb19b',
+        //     cancelButtonColor: '#d33',
+        //     confirmButtonText: 'Yes!'
+        // }).then((result) => {
+        //     if (result.isConfirmed) {
+        //         handleLogout()
+        //     }
+        // })
+    }
+
+    // let handleLogout = () => {
+
+    //     localStorage.removeItem('token')
+    //     window.location = '/home'
+
+    // }
+
+    // useEffect(() => {
+    //     checkuser();
+    // }, []);
+
+    return (
+        <>
+            <nav class="navbar navbar-expand-lg ">
+                <div class="container-fluid">
+                    <h1> Hi user </h1>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse " id="navbarText">
+                        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                            <li class="nav-item" onClick={LogOutprompt}>
+                                <span class="nav-link Nav-header" aria-current="page" >Log out</span>
+                            </li>
+                            <li class="nav-item">
+                                <span class="nav-link Nav-header" aria-current="page" >About Me</span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+
+            <div className='fix-box-nav'>
+
+            </div>
+
+        </>
+    )
+}
+
+export default Nav
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
+import Nav from '../../components/Nav/Nav';
+import InputBox from '../../components/InputBox/InputBox';
+
+import './Home.css'
 
 function Home() {
   return (
-    <div>
-      Home component text
-    </div>
+    <>
+      <div className="home">
+        <Nav />
+
+        <div className="content col-12 row d-flex justify-content-center">
+
+          <div className="col-6">
+
+            <InputBox />
+
+          </div>
+
+        </div>
+
+
+
+      </div>
+
+
+
+    </>
   )
 }
 
-export default Home
+export default Home;
